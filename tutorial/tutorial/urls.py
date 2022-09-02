5803069338d2f16c6362c9c7a774d0db8436dc68
Kenneth Kimani <kimanim68@gmail.com>
2022-09-02 09:27:02 +0300

    setting up api

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,4 +1,4 @@
-"""movies URL Configuration
+"""tutorial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,10 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework import routers
+from quickstart import views
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', views.movie_list)
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
