7bd62b64902d1d6ec4fb6444d7c0af161619ecad
Kenneth Kimani <kimanim68@gmail.com>
2022-09-06 14:25:42 +0300

    put rate limit to 10

--- movies/movie_api/models.py
+++ movies/movie_api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Rating(models.Model):
-    movie_rating = models.IntegerField()
-    series_rating = models.IntegerField()
+    movie_rating = models.IntegerField(range(1,0))
+    series_rating = models.IntegerField(range(1,10))
     best_scene = models.CharField(max_length=1000)
     worst_scene = models.CharField(max_length=1000)
     favourite_actor = models.CharField(max_length=200)
@@ -20,7 +20,6 @@ class Movies(models.Model):
     length = models.IntegerField()
     description = models.CharField(max_length=500)
     rating = models.ForeignKey(Rating, on_delete=models.CASCADE, related_name="my_movie_rating")
-    price = models.DecimalField(decimal_places=2,max_digits=8)
 
 def __str__(self):
     return f"{self.name}"
